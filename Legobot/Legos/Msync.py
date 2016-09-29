d4dc8090592c453f76eeb4886e8c091d37cbb879
Bren Briggs <briggs.brenton@gmail.com>
2016-09-29 13:17:29 -0400

    I can haz variable interpolation?

--- Legobot/Legos/Msync.py
+++ Legobot/Legos/Msync.py
@@ -34,7 +34,7 @@ class Audit(Lego):
 
     def get_current_msync(self):
         try:
-            msync_version = requests.get('https://raw.githubusercontent.com/voxpupuli/modulesync_config/master/moduleroot/.msync.yml')
+            msync_version = requests.get("%smodulesync_config/master/moduleroot/.msync.yml" % base_url)
             msync_version = msync_version.text
             return msync_version.strip()
         except Exception as e:
