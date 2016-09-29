cd80012eae3dde16ed7e7e8d4bc854a059d17daf
Bren Briggs <briggs.brenton@gmail.com>
2016-09-29 11:35:02 -0400

    Actually send response back to actor in msync lego

--- Legobot/Legos/Msync.py
+++ Legobot/Legos/Msync.py
@@ -14,6 +14,7 @@ class Audit(Lego):
 
         if arg == None:
             response = self.get_current_msync()
+            self.reply(message, response)
 
         elif arg == "getver":
             try:
